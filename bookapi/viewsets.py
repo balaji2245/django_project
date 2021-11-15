2851e96a1eca54dfb36f20a9f5ab0022cea54177
Balaji Davangave <40818500+balaji2245@users.noreply.github.com>
2021-11-15 10:02:36 +0530

    Update viewsets.py

--- bookapi/viewsets.py
+++ bookapi/viewsets.py
@@ -4,6 +4,5 @@ from . import serializers
 
 class BookViewset(viewsets.ModelViewSet):
     queryset = models.Book.objects.all()
-    queryset = queryset+"oooo"
     serializer_class = serializers.BookSerializer
 
