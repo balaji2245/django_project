f70b60f29284ed620e507ce50a2e555852115a80
Balaji Davangave <40818500+balaji2245@users.noreply.github.com>
2021-11-15 11:53:45 +0530

    Update views.py

--- bookapi/views.py
+++ bookapi/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .serializers import BookSerializer
+
+
+class BookList(APIView):
+  def get(self, request):
+    print("requested {{{{{{{{{{{{{{{{{{")
+    
+    my_books = Book.objects.all()
+    serializer = BookSerializer(my_books, many= True)
+    
+    my_dict = {
+      "status_code":200,
+      "stats": "success",
+      "data": serializer.data,
+    }
+    
+    res = Response(my_dict)
+    
+    return res
+ 
+def post(self):
+  pass
+
+
