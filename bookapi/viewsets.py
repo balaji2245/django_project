e73834d2e46db16b2d51041a652880eea44f701a
Balaji Davangave <40818500+balaji2245@users.noreply.github.com>
2021-11-15 13:15:45 +0530

    Update viewsets.py

--- bookapi/viewsets.py
+++ bookapi/viewsets.py
@@ -9,6 +9,6 @@ class BookViewset(viewsets.ModelViewSet):
 #     print("++++++++++++++====")
 #     queryset = dict1
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++{{{{}{}{}{[][][[][][]]")
-    print(models.Book.objects.all()[1])
+    print(queryset.name)
     serializer_class = serializers.BookSerializer
 
