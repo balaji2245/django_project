5a1139f5f0a345905a0ab70436d837d29cdc975c
Balaji Davangave <40818500+balaji2245@users.noreply.github.com>
2021-11-15 10:50:14 +0530

    Update serializers.py

--- bookapi/serializers.py
+++ bookapi/serializers.py
@@ -4,6 +4,7 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        print(model)
         fields = '__all__'
 #         dict = {"status_code": 200, "status": "success", "data": fields}
 #         fields = dict
