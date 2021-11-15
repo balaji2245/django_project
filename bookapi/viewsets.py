5df3ec4eddbbdc355ac12fc822d50cdb373955b9
Balaji Davangave <40818500+balaji2245@users.noreply.github.com>
2021-11-15 10:47:19 +0530

    Update viewsets.py

--- bookapi/viewsets.py
+++ bookapi/viewsets.py
@@ -5,9 +5,9 @@ from . import serializers
 class BookViewset(viewsets.ModelViewSet):
     queryset = models.Book.objects.all()
     
-    dict = {"status_code": 200, "status": "success", "data": queryset}
-    print("++++++++++++++")
-    queryset = dict
+#     dict = {"status_code": 200, "status": "success", "data": queryset}
+    print("++++++++++++++====")
+#     queryset = dict
     print(queryset)
     serializer_class = serializers.BookSerializer
 
